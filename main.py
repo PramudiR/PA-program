9e2eef5e17e1a2528a2e0b3d30fb6d1866ca3b65
Pro59 <108863613+PramudiR@users.noreply.github.com>
2022-09-08 06:14:36 +0000

    Google web search feature.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import os
-import discord #importing the library
-import requests #to make HTTP requests
-import json #to make JSON requests
-import random #to select messages randomly.
-from replit import db #to use the replit db
+import discord  #importing the library
+import requests  #to make HTTP requests
+import json  #to make JSON requests
+import random  #to select messages randomly.
+from replit import db  #to use the replit db
 from discord.ext import tasks
 from serpapi import GoogleSearch
-
 
 my_secret = os.environ['Token']
 intents = discord.Intents.default()
@@ -14,119 +13,152 @@ intents.message_content = True
 intents.messages = True
 client = discord.Client(intents=intents)
 
-sad_words = ['sad', 'depressed', 'stressed', 'unhappy', 'angry',
-            'miserable', 'depressing', 'stressing', 'rude']
+sad_words = [
+    'sad', 'depressed', 'stressed', 'unhappy', 'angry', 'miserable',
+    'depressing', 'stressing', 'rude'
+]
 
 encouragements = [
-  "Cheer up!", "Hang in there.", "You are a great soul!",
-  "Great days will come."
+    "Cheer up!", "Hang in there.", "You are a great soul!",
+    "Great days will come."
 ]
 
 if "responding" not in db.keys():
-  db["responding"] = True
+    db["responding"] = True
+
 
 def get_quotes():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " - " + json_data[0]['a']
-  return(quote)
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " - " + json_data[0]['a']
+    return (quote)
+
 
 def update_encouragements(encouraging_input):
-  if "encouragements" in db.keys():
-    encourage = db["encouragements"]
-    encourage.append(encouraging_input)
-    db["encouragements"] = encourage
-  else:
-    db["encouragements"] = [encouraging_input]
+    if "encouragements" in db.keys():
+        encourage = db["encouragements"]
+        encourage.append(encouraging_input)
+        db["encouragements"] = encourage
+    else:
+        db["encouragements"] = [encouraging_input]
+
 
 def delete_message(index):
-  encourage = db["encouragements"]
-  if len(encourage) > index:
-    del encourage[index]
-    db["encouragements"] = encourage
+    encourage = db["encouragements"]
+    if len(encourage) > index:
+        del encourage[index]
+        db["encouragements"] = encourage
+
 
 status = ['Bot Playground', 'with Python']
 
-@tasks.loop(seconds = 600)
+
+@tasks.loop(seconds=600)
 async def change_status():
-  await client.change_presence(activity = discord.Game(next(status)))
+    await client.change_presence(activity=discord.Game(next(status)))
+
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
-  msg = message.content
-  
-  if message.author == client.user: return
+    msg = message.content
 
-  if msg.startswith(('hello', 'hi', 'Hello', 'Hello!', 'hey')):
-    await message.channel.send('Hello!')
+    if message.author == client.user: return
 
-  if msg.startswith('$inspire'):
-    quote = get_quotes()
-    await message.channel.send(quote)
+    if msg.startswith(('hello', 'hi', 'Hello', 'Hello!', 'hey')):
+        await message.channel.send('Hello!')
 
-  if db["responding"]:
-    options = encouragements
-    if "encouragements" in db.keys():
-      options = options + db["encouragements"].value
-    
-    if any(word in msg for word in sad_words):
-      await message.channel.send(random.choice(options))
+    if msg.startswith('$inspire'):
+        quote = get_quotes()
+        await message.channel.send(quote)
 
-  if msg.startswith("$new"):
-    encouraging_input = msg.split("$new ", 1)[1]
-    update_encouragements(encouraging_input)
-    await message.channel.send("New encouraging message added.")
+    if db["responding"]:
+        options = encouragements
+        if "encouragements" in db.keys():
+            options = options + db["encouragements"].value
 
-  if msg.startswith("$del"):
-    encourage = []
-    if "encouragements" in db.keys():
-      index = int(msg.split("$del", 1)[1])
-      delete_message(index)
-      encourage = db["encouragements"].value
-    await message.channel.send(encourage)
+        if any(word in msg for word in sad_words):
+            await message.channel.send(random.choice(options))
 
-  if msg.startswith("$list"):
-    encourage = []
-    if "encouragements" in db.keys():
-      encourage = db["encouragements"].value
-    await message.channel.send(encourage)
+    if msg.startswith("$new"):
+        encouraging_input = msg.split("$new ", 1)[1]
+        update_encouragements(encouraging_input)
+        await message.channel.send("New encouraging message added.")
 
-  if msg.startswith("$responding"):
-    val = msg.split("$responding ", 1)[1]
+    if msg.startswith("$del"):
+        encourage = []
+        if "encouragements" in db.keys():
+            index = int(msg.split("$del", 1)[1])
+            delete_message(index)
+            encourage = db["encouragements"].value
+        await message.channel.send(encourage)
 
-    if val.lower() == "on":
-      db["responding"] = True
-      await message.channel.send("Responding is ON.")
-    else:
-      db["responding"] = False
-      await message.channel.send("Responding is OFF.")
+    if msg.startswith("$list"):
+        encourage = []
+        if "encouragements" in db.keys():
+            encourage = db["encouragements"].value
+        await message.channel.send(encourage)
 
-  if msg.startswith("$show"):
-    val = msg.split("$show", 1)[1]
-    
-    my_secret = os.environ['API key']
-    params = {
-      "api_key": my_secret,
-      "engine": "google",
-      "q": val,
-      "hl": "en",
-      "tbm": "isch"
-    }
-    search = GoogleSearch(params)
-    results = search.get_dict()
-    image_results = []
+    if msg.startswith("$responding"):
+        val = msg.split("$responding ", 1)[1]
 
-    for image in results["images_results"]:
-      image_results.append({
-        "original": image["original"]
-        })
-    
-    for j in range(1,6):
-      await message.channel.send(image_results[j]["original"])
-    await message.channel.send("Have any more questions:question:\nFeel free to ask again :smiley: !")
+        if val.lower() == "on":
+            db["responding"] = True
+            await message.channel.send("Responding is ON.")
+        else:
+            db["responding"] = False
+            await message.channel.send("Responding is OFF.")
+
+    if msg.startswith("$show"):
+        val = msg.split("$show", 1)[1]
+
+        my_secret = os.environ['API key']
+        params = {
+            "api_key": my_secret,
+            "engine": "google",
+            "q": val,
+            "hl": "en",
+            "tbm": "isch"
+        }
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        image_results = []
+
+        for image in results["images_results"]:
+            image_results.append({"original": image["original"]})
+
+        for j in range(1, 7):
+            await message.channel.send(image_results[j]["original"])
+        await message.channel.send(
+            "Here you go.\nHave any more questions:question:\nFeel free to ask again :smiley: !"
+        )
+
+    if msg.startswith("$find"):
+        val = msg.split("$find", 1)[1]
+
+        my_secret = os.environ['API key']
+        params = {
+            "api_key": my_secret,
+            "engine": "google",
+            "q": val,
+            "hl": "en"
+        }
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        web_results = []
+
+        for link in results["organic_results"]:
+            web_results.append({"link": link["link"]})
+
+        for j in range(1, 7):
+            await message.channel.send(web_results[j]["link"])
+        await message.channel.send(
+            "Here you go.\nHave any more questions:question:\nFeel free to ask again :smiley: !"
+        )
+
 
 client.run(my_secret)
